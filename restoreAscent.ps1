7a26db36224353bef88902b7f91cc33dc2e07f16
Neil Kiely <neilkiely@outlook.com>
2021-08-04 09:28:10 +0100

    fixed: restore

--- restoreAscent.ps1
+++ restoreAscent.ps1
@@ -2,4 +2,4 @@
 
 $full_path = $backup_folder_path + $backup_you_want_to_restore
 
-Expand-Archive -Path $full_path -DestinationPath "C:\Users\$env:USERNAME\AppData\Local\Packages\CurveDigital.TheAscent_1ezqdnbhnc70m\SystemAppData\wgs"
+Expand-Archive -Path $full_path -DestinationPath "C:\Users\$env:USERNAME\AppData\Local\Packages\CurveDigital.TheAscent_1ezqdnbhnc70m\SystemAppData" -Force
--- variables.ps1
+++ variables.ps1
@@ -3,7 +3,7 @@ $backup_folder_path = "C:\Users\$env:USERNAME\Desktop\$backups_folder_name\"
 Write-Output $backup_folder_name
 
 #Paste the name of the zip file you want to restore here!
-$backup_you_want_to_restore = "TheAscentBackup_03082021_233619.zip"
+$backup_you_want_to_restore = "TheAscentBackup_04082021_092042.zip"
 
 $save_game_path = "C:\Users\$env:USERNAME\AppData\Local\Packages\CurveDigital.TheAscent_1ezqdnbhnc70m\SystemAppData\wgs"
 $restore_path = "C:\Users\$env:USERNAME\AppData\Local\Packages\CurveDigital.TheAscent_1ezqdnbhnc70m\SystemAppData"
